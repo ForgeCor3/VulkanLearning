3c7139cc8e732b5f685d85016c981bed27cd3bf8
ForgeCor3 <forgecor3@gmail.com>
2023-12-05 22:01:38 +0100

    04-SimpleRayTracer [GraphicsPipeline init]

---
+++ 04-SimpleRaytracer/include/Vulkan/VulkanGraphicsPipeline.h
@@ -0,0 +1,21 @@
+#ifndef VULKANGRAPHICSPIPELINE_H
+#define VULKANGRAPHICSPIPELINE_H
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "GlobUtils.h"
+
+class VulkanGraphicsPipeline final
+{
+public:
+    DISABLE_COPY_AND_MOVE(VulkanGraphicsPipeline)
+
+    VulkanGraphicsPipeline();
+    ~VulkanGraphicsPipeline();
+
+private:
+
+};
+
+#endif // VULKANGRAPHICSPIPELINE_H
